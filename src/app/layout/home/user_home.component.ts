ae9366589b219d4103f767c60c983ba3d2079951
NourDeen89 <deennurr@gmail.com>
2018-04-26 12:57:20 +0200

    ajout de formulaires de connexion et de creation de compte carmes

---
+++ src/app/layout/home/user_home.component.ts
@@ -0,0 +1,18 @@
+import { Component , OnInit } from '@angular/core';
+
+
+@Component({
+    templateUrl: './user_home.component.html',
+})
+
+export class UserhomeComponent implements OnInit {
+
+    constructor () {
+
+    }
+
+
+    ngOnInit () {
+
+    }
+}
